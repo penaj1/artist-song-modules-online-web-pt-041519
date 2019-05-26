ca99c59760ceecf416e5737668e17c9514c21b93
Janice Pena <jipena94@gmail.com>
2019-05-26 16:31:42 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,15 +1,17 @@
 module Memorable
-  
   module ClassMethods
-    
-    def self.reset_all
+    def reset_all
       self.all.clear
     end
-
-    def self.count
+ 
+    def count
       self.all.count
     end
-    
   end
-
+ 
+  module InstanceMethods
+    def initialize
+      # some more code coming soon!
+    end
+  end
 end
