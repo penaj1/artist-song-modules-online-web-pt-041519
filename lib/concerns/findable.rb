8ba0ead7594d79830c63bc61ce764359320c632a
Janice Pena <jipena94@gmail.com>
2019-05-26 16:25:21 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,8 +3,10 @@ module Findable
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
   end
-
-
+  
+  def self.find_by_name(name)
+    @@songs.detect{|a| a.name == name}
+  end
 
 
 end
