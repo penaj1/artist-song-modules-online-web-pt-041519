decb8e59d08323b82774835ee48889a6240279a0
Janice Pena <jipena94@gmail.com>
2019-05-26 16:34:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,10 +12,10 @@ class Artist
 
   @@artists = []
 
-  #def initialize
+  def initialize
     #@@artists << self
     #@songs = []
-  #end
+  end
   
   #def self.find_by_name(name)
     #@@artists.detect{|a| a.name == name}
